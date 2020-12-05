75326fa891de26ad93557649d6c9914a9541e857
RyanSaxe <ryancsaxe+1@gmail.com>
2020-12-05 00:40:36 -0500

    added ability to add interactions between columns and created validation file

--- simval/simulators.py
+++ simval/simulators.py
@@ -7,38 +7,32 @@ import inspect
 class DataSimulator:
     def __init__(
         self,
-        columns: dict = None,
+        instructions: dict = None,
+        interactions: dict = None,
     ):
-        if columns is None:
-            columns = {
+        if instructions is None:
+            instructions = {
                 'simulation': dict()
             }
-        self.instructions = {c:dict() for c in columns.keys()}
         defaults = self._get_defaults()
         assert 'distribution' in defaults, 'Must have a default manner of creating data distribution'
-        for column,rules in columns.items():
+        self.interactions = interactions
+        for name,rules in instructions.items():
             rules = {**defaults, **rules}
-            self.instructions[column]['priority_q'] = rules.pop('priority_q',[])
-            for name, build in rules.items():
-                self._set_instructions(column, name, build)
+            self.instructions[name] = rules
 
     def _get_defaults(self) -> dict:
-        raise NotImplementedError("Override Me! Don't forget to include 'distribution', as it is required")
+        #overwrite this often
+        return {
+            'distribution': np.random.normal,
+            'priority_q': []
+        }
 
-    def _set_instructions(self, column: str, name: str, build: Union[tuple, list, Callable]) -> None:
-        self.instructions[column][name] = dict()
-        if not isinstance(build,(tuple, list)):
-            assert callable(build), 'the function given is not callable'
-            self.instructions[column][name]['f'] = build
-            self.instructions[column][name]['kwargs'] = dict()
-        else:
-            assert len(build) == 2, 'build must be a tuple of (function, kwargs)'
-            assert isinstance(build[1], dict), 'the second build element must be a dictionary'
-            assert callable(build[0]), 'the function given is not callable'
-            self.instructions[column][name]['f'] = build[0]
-            self.instructions[column][name]['kwargs'] = build[1]
+    def simulate(self, size: int) -> pd.DataFrame:
+        df = self._create_base(size)
+        return self._apply_interactions(df)
             
-    def create_dataframe(self, size: int) -> pd.DataFrame:
+    def _create_base(self, size: int) -> pd.DataFrame:
         data_dict = dict()
         for column, rules in self.instructions.items():
             data = rules['distribution']['f'](size=size, **rules['distribution']['kwargs'])
@@ -54,27 +48,36 @@ class DataSimulator:
             data_dict[column] = data
         return pd.DataFrame(data_dict)
 
-class SimpleSalesSimulator(DataSimulator):
-    def __init__(
-        self,
-        columns: dict = None,
-        minimum: float = 100.0,
-    ):
-        super().__init__(columns = columns)
-        self.minimum = minimum
+    def _apply_interactions(self, df: pf.DataFrame) -> pd.DataFrame:
+        for column, rules in self.interactions.items():
+            df[column] = None
+            preprocessed = ['priority_q']
+            for name in rules['priority_q']:
+                interaction = rules[name]
+                df[column] = self._create_interaction(interaction, df)
+                preprocessed.append(name)
+            for name, interaction in self.interactions.items():
+                if name in preprocessed:
+                    continue
+                df[column] = self._create_interaction(interaction, df)
+        return df
 
-    def _get_defaults(self) -> dict:
-        return {
-            'distribution': (
-                self._distribution, {
-                    'loc':0.0,
-                    'scale':0.1
-                }
-            ),
-            'flip_negative': np.absolute,
-            'set_minimum': lambda x: (x + 1) * minimum,
-            'priority_q': ['flip_negative','set_minimum']
-        }
+    def _create_interaction(self, interaction: dict, df: pd.DataFrame):
+        function = interaction['f']
+        cols = interaction['cols']
+        kwargs = interaction.get('kwargs',dict())
+        split = interaction.get('split',False)
+        axis = interaction.get('axis',None)
+        if axis is None:
+            output = function(
+                *[df[c] for c in cols] if split else df[cols],
+                **kwargs
+            )
+        else:
+            output = df[cols].apply(lambda x: function(
+                    *x if split else x,
+                    **kwargs
+                ), axis=axis
+            )
+        return output
 
-    def _distribution(self, size, loc, scale):
-         return np.cumsum(np.random.normal(size=size, loc=loc, scale=scale))
---
+++ simval/validators.py
@@ -0,0 +1,9 @@
+from simulators import DataSimulator
+
+class Validator:
+    def __init__(
+        self,
+        simulator: DataSimulator,
+        model
+    ):
+        pass
